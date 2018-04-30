1ba0f47d61b6c1e510b81ee4bac1c432b7537e43
corneyeski <alanvallve@gmail.com>
2018-04-30 19:08:26 +0200

    as

--- app/src/main/java/curso/kotlin/holamundoandroidkotlin/CreditosActivity.kt
+++ app/src/main/java/curso/kotlin/holamundoandroidkotlin/CreditosActivity.kt
@@ -1,8 +1,10 @@
 package curso.kotlin.holamundoandroidkotlin
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import android.widget.Toast.makeText
 
 class CreditosActivity : AppCompatActivity() {
 
@@ -13,8 +15,12 @@ class CreditosActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        Toast.makeText(this, "finalizado", Toast.LENGTH_LONG).show()
+        val toast = Toast(this)
+        toast.mostrarMensaje(this, "finalizado")
     }
 }
 
-fun Toast.mostrarMensaje(){}
+fun Toast.mostrarMensaje(ctx:Context, info:String){
+    makeText(ctx, info, Toast.LENGTH_LONG).show()
+
+}
